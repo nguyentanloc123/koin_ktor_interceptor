44a086f344623f4036983de9bc3920d7a8aedde2
nguyentanloc <locn049@gmail.com>
2021-07-05 17:45:41 +0700

    feat: add ktor and koin lib

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+    suspend fun runKtor()
+    {
+        val client = HttpClient(CIO)
+        val response: HttpResponse = client.get("https://ktor.io/")
+        println(response.status)
+        client.close()
     }
 }
