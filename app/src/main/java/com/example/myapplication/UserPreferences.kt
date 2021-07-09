17980f74eba450c2a673fa673d7d8b0be1b818da
Nguyen Nam <itnams12@gmail.comm>
2021-07-09 14:58:39 +0700

    update lib

--- app/src/main/java/com/example/myapplication/UserPreferences.kt
+++ app/src/main/java/com/example/myapplication/UserPreferences.kt
@@ -2,11 +2,12 @@ package com.example.myapplication
 
 
 import android.content.Context
-import androidx.datastore.DataStore
-import androidx.datastore.preferences.Preferences
-import androidx.datastore.preferences.createDataStore
-import androidx.datastore.preferences.edit
-import androidx.datastore.preferences.preferencesKey
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -15,26 +16,34 @@ class UserPreferences(
 ) {
     companion object {
         private const val DATA_STORE_NAME = "user_data_store"
-        private val KEY_AUTH = preferencesKey<String>("key_auth")
+        private val KEY_AUTH = intPreferencesKey("key_auth")
     }
 
     private val appContext = context.applicationContext
-    private val dataStore: DataStore<Preferences>
+//    private val dataStore: DataStore<Preferences>
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "accesstoken")
 
-    init {
-        dataStore = appContext.createDataStore(
-            name = DATA_STORE_NAME
-        )
-    }
-
-    val authToken: Flow<String?>
-        get() = dataStore.data.map { preferences ->
-            preferences[KEY_AUTH]
-        }
+    val EXAMPLE_COUNTER = stringPreferencesKey("example_counter")
+    val authToken: Flow<String> = context.dataStore.data
+        .map { preferences ->
+            // No type safety.
+            preferences[EXAMPLE_COUNTER] ?: 0
+        } as Flow<String>
 
     suspend fun saveAuthToken(authToken: String) {
-        dataStore.edit { preferences ->
-            preferences[KEY_AUTH] = authToken
+        appContext.dataStore.edit { settings ->
+            val currentCounterValue = settings[EXAMPLE_COUNTER] ?: 0
+            settings[EXAMPLE_COUNTER] = authToken
         }
     }
+//    val authToken: Flow<String?>
+//        get() = dataStore.data.map { preferences ->
+//            preferences[KEY_AUTH]
+//        }
+
+//    suspend fun saveAuthToken(authToken: String) {
+//        dataStore.edit { preferences ->
+//            preferences[KEY_AUTH] = authToken
+//        }
+//    }
 }
