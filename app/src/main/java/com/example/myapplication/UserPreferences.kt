7ab8dbc00d6477faaba8f98c73dd37bef98bc792
Nguyen Nam <itnams12@gmail.comm>
2021-07-12 13:36:39 +0700

    fix data store

--- app/src/main/java/com/example/myapplication/UserPreferences.kt
+++ app/src/main/java/com/example/myapplication/UserPreferences.kt
@@ -12,7 +12,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-
+var loggedInScope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "access_token",scope = loggedInScope)
 class UserPreferences(
     context: Context
 ) {
@@ -20,9 +21,6 @@ class UserPreferences(
         private const val DATA_STORE_NAME = "user_data_store"
         private val KEY_AUTH = stringPreferencesKey("key_auth")
     }
-    var loggedInScope: CoroutineScope = CoroutineScope(Dispatchers.Default)
-
-    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "access_token",scope = loggedInScope)
     private val appContext = context.applicationContext
     val EXAMPLE_KEY = stringPreferencesKey("key_auth")
     val EXAMPLE_COUNTER = stringPreferencesKey("example_counter")
